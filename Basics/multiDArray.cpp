5ce7d6d0608eac1aeef121512ab4507b2bcdb2e8
s-tushar <tusharsonawane2837@gmail.com>
2020-11-21 19:09:17 +0530

    done with arrays

--- Basics/multiDArray.cpp
+++ Basics/multiDArray.cpp
@@ -6,7 +6,20 @@
 using namespace std;
 
 int main() {
-    
+    // let's try on arrays
+    // stored in memory in rowmajor order
+    int arr[3][2] = {
+                        {10, 20},
+                        {30, 40},
+                        {50, 60} 
+                    };
+
+    for(int i = 0; i<3; i++){
+        for(int j=0; j<2; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 
     return 0;
